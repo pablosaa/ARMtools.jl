646f44a048f73d8fabfa48e2e3a8e27fe75e8623
Pablo Saavedra G <pablosaa@uni-bonn.de>
2021-04-26 21:23:14 +0200

    merged confict fixed

--- src/ARMtools.jl
+++ src/ARMtools.jl
@@ -1,14 +1,6 @@
-<<<<<<< HEAD
 module ARMtools
 
-greet() = print("Hello World!")
 
 end # module
-=======
 # Main file containing the package module
 # See LICENSE
-
-module ARMtools
-
-end
->>>>>>> 628437097362e27d917677a8bd4c4b22595148e0
