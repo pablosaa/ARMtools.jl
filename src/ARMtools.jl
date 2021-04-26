11072fd87215b109d27baab26e2aa28af765dfb4
Pablo Saavedra-Garfias <pablosaa@uni-bonn.de>
2021-04-27 01:49:51 +0200

    Update ARMtools.jl

--- src/ARMtools.jl
+++ src/ARMtools.jl
@@ -1,3 +1,16 @@
+"""
+Julia Package including functions to read and process data from the DOE ARM program.
+
+The package is mainly focused to work with instruments from the NSA facility in Utqiagvik, Alaska.
+Nonetheless since most of the ARM instrumentation is standard, the package can also be used for
+other facilities.
+
+(c) 2020, Pablo Saavedra Garfias
+University of Leipzig
+Faculty of Physics and Geosciences
+
+See LICENSE
+"""
 module ARMtools
 
 
