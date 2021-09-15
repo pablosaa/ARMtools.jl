0a53b80ada7bae4e7a3bc8c4c92572a7d5f4ac04
Pablo Saavedra G <pablosaa@uni-bonn.de>
2021-09-15 22:51:54 +0200

    function for spectral noise extractor included

--- src/kazr.jl
+++ src/kazr.jl
@@ -136,10 +136,11 @@ function readSPECCOPOL(kazr_file::String; addvars=[], onlyvars=[], attvars=[])
                       :spect_mask=>"locator_mask")
 
         attvars = Dict(:nyquist_vel => "nyquist_velocity",
-                       :cal_constant => "cal_constant",
+                       :calib_const => "cal_constant",
                        :Nfft => "fft_len",
                        :noise => "rx_noise",
                        :frequency => "radar_operating_frequency",
+                       :Nspec_ave => "num_spectral_averages",
         )
     elseif isvariablein(kazr_file, "radar_power_spectrum_of_copolar_h")
         ncvars = Dict(:time=>"time",
@@ -176,45 +177,79 @@ end
 
 
 # *************************************************************************************
+# Following Hildebrand P.H. and Sekhon R.S. (1974) Paper:
+# "Objective Determination of the Noise Level in Doppler Spectra"
+# Journal of Applied Meteorology, Vol 13.
 #
-
-function η_dBz(spec::Dict)
-
-    # calculating spectral reflectivity:
-    (nh, nt) = size(spec[:spect_mask])
-    Pₙ = spec[:noise][1]    # dBm
-    C  = spec[:cal_constant][1]   # dB
-    η_out = similar(spec[:η_hh])
-    
-    for it ∈ (1:nt)
-        for ih ∈ (1:nh)
-            k = spec[:spect_mask][ih, it]
-            if k ≥ 0
-                k += 1
-            else
-                continue
-            end
-            
-            let range_factor = 20log10(1f-3spec[:height][ih]),
-                    Pₛ = spec[:η_hh][:, k]  # dBm
-            
-                    Pₜ = Pₛ .- Pₙ    # dB
-                    Znn = @. Pₜ + range_factor - C # dB
-                    η_out[:,k] = denoise(Znn, wavelet(WT.sym6, WT.Filter), dnt=VisuShrink(256), TI=true)
-                
-            end
-        end
-    end
-    return η_out
-end
-function η_dBz(filen::String)
-    @assert isfile(filen) "$filen seems not to exist!"
-    spec = ARMtools.readSPECCOPOL(filen)
-    spec[:Zη] = η_dBz(spec)
-    return spec
+function Extract_Spectra_NL(Zη::Matrix; p::Int=1)
+	#p = Zη[:Nspec_ave][1]
+	
+	# Defining output variables:
+	Zη_cor = similar(Zη)
+	SNR_dB = similar(Zη)
+	
+	Nspec, Nsamples = size(Zη)
+	for idx ∈ 1:Nsamples
+		# spectral reflectivity in linear units:
+		S = 10f0.^(0.1Zη[:, idx])
+		
+		# sorting S from smallest to largest:
+		Iₙ = sortperm(S)
+		Sₙ = S[Iₙ]
+		
+		ΣSₙ = cumsum(Sₙ, dims=1)
+		N = cumsum(isfinite.(Sₙ), dims=1)
+		fₙ = Iₙ./Nspec
+		
+		# eq. (4) :
+		σ² = cumsum(Sₙ.*fₙ.^2, dims=1)./ΣSₙ .- (cumsum(Sₙ.*fₙ,dims=1)./ΣSₙ).^2
+		
+		# eq. (5)
+		σₙ² = 1/12 #(F.^2)/12
+		
+		# eq. (6) :
+		P = ΣSₙ./N
+		
+		# eq. (7) :
+		Q = cumsum(Sₙ.^2, dims=1)./N .- P.^2
+		
+		# eq. (8) :
+		R₁ = σₙ²./σ²
+		
+		# eq. (9) :
+		R₂ = (P.^2)./(Q*p)
+		
+		# Applying criteria to determine Noise Levels
+		# * For R₂ :
+		Inoise2, Pnoise2 = findfirst(R₂ .≤ 1) |> x->!isnothing(x) ? (x, P[x]) : (10, Sₙ[10])
+		
+		Qnoise2 = Q[Inoise2]
+				
+		# For R₁ :
+		Inoise1, Pnoise1 = findlast(0 .≤ diff(R₁) .< .01) |> x->!isnothing(x) ? (x, P[x]) : (10,Sₙ[10])
+		Qnoise1 = Q[Inoise1]
+				
+		# Correcting spectral reflectivity from noise level:
+		Zη_cor[:, idx], SNR_dB[:, idx] = let tmp = (S .- Pnoise2)
+			tmp[tmp.≤0] .= NaN
+			ii = tmp .≤ Pnoise2 + sqrt(Qnoise2)
+			NoiseMean = mean(tmp[ii])	
+			NoisePeak = maximum(tmp[ii])
+			NoiseStdv = std(tmp[ii])
+			tmp[(tmp .≤ NoisePeak) .| isnan.(tmp)] .= Pnoise2
+			
+			# Signal-to-noise-ratio:
+			SNR = tmp./Pnoise2
+			
+			10log10.(tmp), 10log10.(SNR)
+		end
+	end
+	return Zη_cor, SNR_dB
 end
 # ----/
 
+# ********************************************************
+# Integrate spectral reflectivity
 function ∫zdη(η::AbstractVector; i0=1, i1=length(η))
     i0 = max(i0, 1)
     i1 = min(i1, length(η))
@@ -226,3 +261,40 @@ end
 # ----/
 
 # end of script.
+# *****************************************************************
+##function η_dBz(spec::Dict)
+##
+##    # calculating spectral reflectivity:
+##    (nh, nt) = size(spec[:spect_mask])
+##    Pₙ = spec[:noise][1]    # dBm
+##    C  = spec[:cal_constant][1]   # dB
+##    η_out = similar(spec[:η_hh])
+##    
+##    for it ∈ (1:nt)
+##        for ih ∈ (1:nh)
+##            k = spec[:spect_mask][ih, it]
+##            if k ≥ 0
+##                k += 1
+##            else
+##                continue
+##            end
+##            
+##            let range_factor = 20log10(1f-3spec[:height][ih]),
+##                    Pₛ = spec[:η_hh][:, k]  # dBm
+##            
+##                    Pₜ = Pₛ .- Pₙ    # dB
+##                    Znn = @. Pₜ + range_factor - C # dB
+##                    η_out[:,k] = denoise(Znn, wavelet(WT.sym6, WT.Filter), dnt=VisuShrink(256), TI=true)
+##                
+##            end
+##        end
+##    end
+##    return η_out
+##end
+##function η_dBz(filen::String)
+##    @assert isfile(filen) "$filen seems not to exist!"
+##    spec = ARMtools.readSPECCOPOL(filen)
+##    spec[:Zη] = η_dBz(spec)
+##    return spec
+##end
+### ----/
